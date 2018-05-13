6008b96d1c2da84d25f54fc10900481657cac649
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-13 23:09:42 +0300

    gg

---
+++ day_6_home_work/day_6_home_work/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace day_6_home_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
