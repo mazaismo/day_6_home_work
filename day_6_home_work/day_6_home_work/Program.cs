332a5deb04f0776bf9335fe90d0a176654f4c808
Modris Ābrams <brainstormer57@googlemail.com>
2018-05-14 00:15:27 +0300

    trešais uzdevums

--- day_6_home_work/day_6_home_work/Program.cs
+++ day_6_home_work/day_6_home_work/Program.cs
@@ -6,9 +6,9 @@ namespace day_6_home_work
     {
         static void Main(string[] args)
         {
-            
+            /*
             string y = Console.ReadLine();
-            int z = Convert.ToInt32(y);
+            int z = Convert.ToInt16(y);
             
             
             for (int x = 1; x < 11; x++) 
@@ -16,7 +16,12 @@ namespace day_6_home_work
                 Console.WriteLine(x * z);
                 
             }
+            */
+            for (int x = 10; x > 0; x--)
+            {
+                Console.WriteLine(x);
 
+            }
 
 
             Console.ReadLine();
